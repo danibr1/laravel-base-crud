d857fea21a215a085e651505447b2a4f9f6bcf1c
danibr1 <75913820+danibr1@users.noreply.github.com>
2021-06-15 00:27:39 +0200

    index table completed

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,8 +1,36 @@
 @extends('layout.main')
 
 @section('content')
+
     <div class="container">
-        <h1>cCOMICS</h1>
+        <h1 class="mb-5">COMICS</h1>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Price</th>
+                    <th colspan="3">Action</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($comics as $comic)
+                    <tr>
+                        <td>{{ $comic->id }}</td>
+                        <td>{{ $comic->title }}</td>
+                        <td>{{ $comic->price }}</td>
+                        <td>SHOW</td>
+                        <td>EDIT</td>
+                        <td>DELETE</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+
+
     </div>
 
 @endsection
