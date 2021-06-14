34afabf17ce51bc25171522f3bd744d905ae11ee
danibr1 <75913820+danibr1@users.noreply.github.com>
2021-06-14 23:01:19 +0200

    seeder completed

--- database/migrations/2021_06_14_203523_create_comics_table.php
+++ database/migrations/2021_06_14_203523_create_comics_table.php
@@ -15,6 +15,14 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+                $table->string('title', 50);
+                $table->string('slug');
+                $table->text('description');
+                $table->text('thumb');
+                $table->decimal('price',6,2);
+                $table->string('series', 50);
+                $table->date('sale_date');
+                $table->string('type', 50);
             $table->timestamps();
         });
     }
